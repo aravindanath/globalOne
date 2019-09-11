6d66ea6c9a591a6d3de484a9d308784b50eb008a
aravindanath <aravindanath86@gmail.com>
2019-09-11 08:35:20 +0530

    Drag Drop

--- src/test/java/SelAdv/ActionsClass.java
+++ src/test/java/SelAdv/ActionsClass.java
@@ -1,6 +1,7 @@
 package SelAdv;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
@@ -10,18 +11,50 @@ import Day1.LaunchBrowser;
 public class ActionsClass extends LaunchBrowser {
 
 	@Test
-	public void mouseHover() throws InterruptedException {
+	public void mouseHoverTest() throws InterruptedException {
 
 		driver.get("https://www.amazon.com");
 
 		WebElement tgt = driver.findElement(By.xpath("//*[text()='Departments']"));
-		
-		Actions act =  new Actions(driver);
-		act.moveToElement(tgt).build().perform();
+		mouseHover(driver, tgt);
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//*[@id='nav-flyout-shopAll']/div[2]/a[12]/span")).click();
-		//*[text()='Women's Fashion']
+
+	}
+
+	@Test
+	public void dragDropTest() throws InterruptedException {
+
+		driver.get("http://demo.guru99.com/test/drag_drop.html");
+		WebElement src1 = driver.findElement(By.xpath("(//*[@id='fourth']/a)[1]"));
+		WebElement tgt1 = driver.findElement(By.cssSelector("#amt8"));
+
+		WebElement src2 = driver.findElement(By.xpath("//*[@id='credit1']/a"));
+		WebElement tgt2 = driver.findElement(By.cssSelector("#loan"));
 		
+		dragDrop(driver, src1, tgt1);
+		dragDrop(driver, src2, tgt2);
+		
+		Thread.sleep(2000);
+	}
+
+	/**
+	 * @author aravindanathdm
+	 * @param driver
+	 * @param element
+	 * 
+	 *            THis method is used for mouse hover
+	 */
+
+	public static void mouseHover(WebDriver driver, WebElement element) {
+		Actions act = new Actions(driver);
+		act.moveToElement(element).build().perform();
+	}
+
+	public static void dragDrop(WebDriver driver, WebElement src, WebElement tgt) {
+		
+		Actions act =  new Actions(driver);
+		act.dragAndDrop(src, tgt).build().perform();
 
 	}
 
